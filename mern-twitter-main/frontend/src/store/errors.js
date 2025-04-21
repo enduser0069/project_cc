a3c38b67296523d0c45ae18c1eea2b869ec3fbc2
enduser0069 <yashahir240106@gmail.com>
2025-04-21 11:45:51 +0530

    Delete mern-twitter-main directory

--- mern-twitter-main/frontend/src/store/errors.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { sessionErrorsReducer } from "./session";
-import { tweetErrorsReducer } from "./tweets";
-
-export default combineReducers({
-  session: sessionErrorsReducer,
-  tweets: tweetErrorsReducer,
-});
